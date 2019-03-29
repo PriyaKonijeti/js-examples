ec2783a4bd2d7879ad6e1dde0523c89a5b6fbf25
priyaKonijeti <sripriya.konijeti@gmail.com>
2019-03-29 22:00:28 +0530

    Example of arrays in js

---
+++ arrays_js_ex.js
@@ -0,0 +1,15 @@
+var movies = ["Jhony", "F2", "118", "Hello"];
+var heros = ["PK", "Venky", "Varun","Tarak", "Akhil"];
+var heroines = ["Renu", "Thamannah", "Mehrin", "Tamannah"];
+var movie = 0;
+console.log("The movie is" + "                    " + movies[movie] );
+for (var movie = 0 ; movie < movies.length; movie++){
+    console.log("priya");
+    console.log(movie);
+    console.log("The movie is" + "" + movies[movie] );
+    console.log(movie);
+
+}
+console.log(movie);
+
+console.log("The movie is" + " " + movies[movie] );
