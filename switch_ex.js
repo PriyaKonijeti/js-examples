1c15e60fab4a0d0116f5977e8a5cb422e5b37c28
priyaKonijeti <sripriya.konijeti@gmail.com>
2019-03-29 22:03:46 +0530

    example for switch

---
+++ switch_ex.js
@@ -0,0 +1,18 @@
+//switch example
+var fruits = "mango";
+switch (fruits) {
+    case 'apple':
+        cosole.log("this fruit colour is red");
+        break;
+    case 'banana':
+        console.log("this fruit colour is yellow");
+        break;
+    case 'mango':
+        console.log("this fruit colour is green");
+        break;
+    case 'orange':
+        console.log("this fruit colour is orange");
+        break;
+    default:
+        console.log("Nothing matched the search phrase");
+}
